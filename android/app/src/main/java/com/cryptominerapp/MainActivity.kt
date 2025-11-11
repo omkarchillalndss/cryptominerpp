4ae46a8d28c67a16e63388d90a1c302f85335c11
omkarchillalndss <omkar.chillal@ndsofttech.com>
2025-11-11 12:01:53 +0530

    app icon changed

--- android/app/src/main/java/com/cryptominerapp/MainActivity.kt
+++ android/app/src/main/java/com/cryptominerapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cryptominerapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +13,15 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "cryptominerapp"
+
+  /**
+   * Called when the activity is starting. Switch from splash theme to app theme.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Switch to AppTheme before calling super to avoid white screen
+    setTheme(R.style.AppTheme)
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
